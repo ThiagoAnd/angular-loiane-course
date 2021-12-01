ad17c6b22c257aef03234fb42929024b203cb7cf
ThiagoAnd <thiago.andrade.it@outlook.com>
2021-12-01 00:37:06 -0300

    Update project

--- Formularios reativos - data driven/projeto-formulario-reativo/src/app/servicos/dropdown.service.ts
+++ Formularios reativos - data driven/projeto-formulario-reativo/src/app/servicos/dropdown.service.ts
@@ -2,6 +2,7 @@ import { HttpClient,HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable(
+
  // {providedIn: 'root'}
 )
 export class DropdownService {
