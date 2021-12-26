056ec9a5a1e9dc0d1b6d804c6b95ae03af1e7fa4
ThiagoAnd <thiago.andrade.it@outlook.com>
2021-12-25 23:43:30 -0300

    Update project

--- Http request/projeto-http-request/src/app/cursos/cursos-form/cursos-form.component.ts
+++ Http request/projeto-http-request/src/app/cursos/cursos-form/cursos-form.component.ts
@@ -11,6 +11,9 @@ export class CursosFormComponent implements OnInit {
   //O formgroup faz com q nosso formulario funcione como uma arvore, ele mapeia
   formulario!: FormGroup;
 
+  //Flag para saber se foi submetido
+  submited = false;
+
   //FormBuilder é necessario para criar form de forma reativa
   constructor(private fb: FormBuilder) { }
 
@@ -21,4 +24,24 @@ export class CursosFormComponent implements OnInit {
     })
   }
 
+  onSubmit(){
+    this.submited = true;
+    console.log(this.formulario.value)
+    this.formulario.valid ? console.log('submit'):null
+  }
+
+  onCancel(){
+    this.submited = false;
+    this.formulario.reset;
+    console.info('cancel')
+  }
+
+  hasErrors(campo: string){
+    console.log('Entrei na validação de erros')
+    console.log(this.formulario.controls[campo].errors?.required)
+    //Returna true se o campo tiver erros
+    //Esse interrogação é o elvis operator, ele é utilizado
+    //para somente verificar o atributo de um objeto apenas se não for nulo
+    return this.formulario.get(campo)?.errors
+  }
 }
