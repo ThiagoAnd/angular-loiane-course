572a3c9622deac17d32dd7e318946aaf09725828
ThiagoAnd <thiago.andrade.it@outlook.com>
2021-12-26 12:55:43 -0300

    Feat #128: HTTP POST Criando curso e criando retorno de pagina

--- Http request/projeto-http-request/src/app/cursos/cursos-form/cursos-form.component.ts
+++ Http request/projeto-http-request/src/app/cursos/cursos-form/cursos-form.component.ts
@@ -1,5 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CursosService } from '../cursos.service';
 
 @Component({
   selector: 'app-cursos-form',
@@ -15,7 +17,7 @@ export class CursosFormComponent implements OnInit {
   submited = false;
 
   //FormBuilder é necessario para criar form de forma reativa
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,private service:CursosService,private location: Location) { }
 
   ngOnInit(): void {
     this.formulario = this.fb.group({
@@ -27,7 +29,20 @@ export class CursosFormComponent implements OnInit {
   onSubmit(){
     this.submited = true;
     console.log(this.formulario.value)
+    //Brincando tentando testar o operador ternario, se for valido console, se não volta null
     this.formulario.valid ? console.log('submit'):null
+
+    if (this.formulario.valid){
+      //com o take(1) que esta no service, não vamos precisar nos desinscrever do observable
+      this.service.create(this.formulario.value).subscribe(
+        sucesso => {alert('Sucesso ao criar curso');
+                    this.location.back();
+      },
+        erro => alert('Erro ao criar curso: '+erro),
+        () => console.log('request completo')
+      )
+    }
+
   }
 
   onCancel(){
@@ -38,7 +53,7 @@ export class CursosFormComponent implements OnInit {
 
   hasErrors(campo: string){
     console.log('Entrei na validação de erros')
-    console.log(this.formulario.controls[campo].errors?.required)
+    console.log('Campo required: '+this.formulario.controls[campo].errors?.required)
     //Returna true se o campo tiver erros
     //Esse interrogação é o elvis operator, ele é utilizado
     //para somente verificar o atributo de um objeto apenas se não for nulo
--- Http request/projeto-http-request/src/app/cursos/cursos.service.ts
+++ Http request/projeto-http-request/src/app/cursos/cursos.service.ts
@@ -2,7 +2,7 @@ import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Curso } from './cursos-lista/curso';
-import { delay, tap } from 'rxjs/operators';
+import { delay, take, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -21,8 +21,12 @@ export class CursosService {
         //Esse pipe a loiane disse que é a forma mais facil para debugar
         //esse .pipe transforma as informações de um obsevable para fazer um output na tela
         .pipe(
-          delay(2000),
+          delay(1000),
           tap(console.log))
     );
+  }
+
+  create(curso: Curso){
+    return this.http.post(this.API,curso).pipe(take(1))
   }
 }
