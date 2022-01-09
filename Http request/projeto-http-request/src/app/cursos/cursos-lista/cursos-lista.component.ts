6202ff487e2b299098af5cac82de3c042e2b5aee
ThiagoAnd <thiago.andrade.it@outlook.com>
2022-01-09 00:49:59 -0300

    Feat #132: Http DELETE: Popup de Confirmação para remover Cursos

--- Http request/projeto-http-request/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ Http request/projeto-http-request/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -11,10 +11,9 @@ import { ActivatedRoute, Router } from '@angular/router';
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
   styleUrls: ['./cursos-lista.component.scss'],
-  preserveWhitespaces:true
+  preserveWhitespaces: true,
 })
 export class CursosListaComponent implements OnInit {
-
   //cursos: Curso[] = [];
 
   //essa exclamação é para estar explicada no one note, verificar
@@ -22,67 +21,72 @@ export class CursosListaComponent implements OnInit {
   //Aqui criamos um subject que é um objeto que consegue emitir valor
   //Sempre que for emitido um erro, vamos emitir um valor de TRUE, por isso vai ser
   //boleano
-  error$ = new Subject <boolean>();
+  error$ = new Subject<boolean>();
 
-  mensagem :string = 'testando mensagem';
-  tipo :string = 'danger';
-
-
+  mensagem: string = 'testando mensagem';
+  tipo: string = 'danger';
 
   //@ViewChild(AlertModalComponent) child!: AlertModalComponent;
   //@ViewChild('elemento') elemento!: ElementRef;
 
-  constructor(private service: CursosService,
-    private router:Router, private route:ActivatedRoute) { }
+  constructor(
+    private service: CursosService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.onRefresh();
   }
 
-  mudar(){
+  mudar() {
     //this.child.mensagem.replace.;
   }
 
-  onRefresh(){
-
-        //essa lista() retorna um observable, e eles são preguiçosos
+  onRefresh() {
+    //essa lista() retorna um observable, e eles são preguiçosos
     //
     //this.service.list()
     //.subscribe(console.log)
     //.subscribe(dados => this.cursos = dados);
 
-    this.cursos$ = this.service.list()
-    .pipe(
-      catchError(
-        error => {
-          console.log("Ocorreu um erro")
-          console.error(error)
-          //Precisamos retornar um observable no catchError
-          //Retornamos empty() ou of() para dizer que retornamos
-          // um vazio, pois de acordo com a logica, é esperado algum retorno
-          //no this.cursos$  e se retornar um erro, ele vai tentar fazer a iteração
-          // e consequentemente vai dar erro no FOR EACH.
-          //Aqui vamos emitir o valor para esse erro que vai ser o true
-          //Emitindo true, ele vai ser capturado no NGIF do pipe async
-          this.error$.next(true);
+    this.cursos$ = this.service.list().pipe(
+      catchError((error) => {
+        console.log('Ocorreu um erro');
+        console.error(error);
+        //Precisamos retornar um observable no catchError
+        //Retornamos empty() ou of() para dizer que retornamos
+        // um vazio, pois de acordo com a logica, é esperado algum retorno
+        //no this.cursos$  e se retornar um erro, ele vai tentar fazer a iteração
+        // e consequentemente vai dar erro no FOR EACH.
+        //Aqui vamos emitir o valor para esse erro que vai ser o true
+        //Emitindo true, ele vai ser capturado no NGIF do pipe async
+        this.error$.next(true);
 
-          //Para mostrar o alerta na pagina, vc coloca ele aqui dentro e tira o
-          //this.error next, o retorn of vc deixa, assim quando der um
-          //erro vai mostrar o alerta, e vc clica no x dele pra fechar.
-          return of();
-
-        }
-      )
+        //Para mostrar o alerta na pagina, vc coloca ele aqui dentro e tira o
+        //this.error next, o retorn of vc deixa, assim quando der um
+        //erro vai mostrar o alerta, e vc clica no x dele pra fechar.
+        return of();
+      })
     );
   }
 
-  onEdit(id:number){
+  onEdit(id: number) {
     //Esse primeiro modo é caso vc não quiser colocar a url inteira, exemplo:
     //cursos/editar, então vc vai ter que dizer que vai navegar, relacionado a sua rota
     //atual, e para isso usamos o route. Ou então vc coloca a url inteira, como no segundo caso
     //que vou deixar ativo
     //this.router.navigate(['editar',id],{relativeTo:this.route});
-    this.router.navigate(['cursos/editar',id])
+    this.router.navigate(['cursos/editar', id]);
   }
 
+  onDelete(curso: Curso) {
+    let escolha = confirm('Gostaria de remover o registro? ');
+   if (escolha){
+     this.service.remove(curso.id).subscribe(
+       sucesso => this.onRefresh(),
+       erro => console.log('Ocorreu um erro')
+     )
+   }
+  }
 }
